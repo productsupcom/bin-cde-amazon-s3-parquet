416c818c35555faf8bf68fd7f98423bf6325985a
khelmrich <kamil.helmrich@productsup.com>
2024-08-02 14:40:25 +0200

    fix stan

--- src/Import/Application/Importer.php
+++ src/Import/Application/Importer.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Productsup\BinCdeAmazonS3Parquet\Import\Application;
 
-use FileConverter;
 use Productsup\BinCdeAmazonS3Parquet\Import\Application\Processor\Processor;
 use Productsup\BinCdeAmazonS3Parquet\Import\Application\Transporter\Transporter;
+use Productsup\BinCdeAmazonS3Parquet\Import\Domain\DuckDb\FileConverter;
 use Productsup\DK\Connector\Application\Feed\OutputFeedForImport;
 use Productsup\DK\Connector\Application\Logger\ConnectorFinished;
 use Productsup\DK\Connector\Application\Logger\ConnectorLogger;
